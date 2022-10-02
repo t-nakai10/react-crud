357061ddf2aa0e307ea98701329944732ce76dff
t-nakai10 <bad.sideline.999@gmail.com>
2022-10-02 21:18:54 +0900

    クエリの実行結果をフロントに出力

--- src/App.js
+++ src/App.js
@@ -6,15 +6,23 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
+    // コレクションを参照.
     const usersCollectionRef = collection(db, "users");
+    // getDocs でクエリを実行し、一つづつ取り出す.
     getDocs(usersCollectionRef).then((querySnapshot) => {
       setUsers(
-        querySnapshot.docs.forEach((doc) => ({ ...doc.data(), id: doc.id }))
+        querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
       );
     });
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
