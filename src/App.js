72dc3e7872d782a43258260f33ed97e93c1b122a
t-nakai10 <bad.sideline.999@gmail.com>
2022-10-02 20:50:45 +0900

    コレクションの参照を取得する

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "firebase.js";
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const usersCollectionRef = collection(db, "users");
+    console.log(usersCollectionRef);
+  }, []);
+
   return <div className="App"></div>;
 }
 
